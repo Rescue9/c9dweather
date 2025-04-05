6a1cd5cc577f872223395919b6c956dfa030a75f
Rescue9 <andrew.buskov@gmail.com>
2025-04-05 12:17:13 +0000

    Gradle & SDK Changed gradle version to 8.13 Changed SDK compile and target version to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.corridor9design.c9dweather"
-    compileSdk = 33
+    compileSdk = 34
     
     defaultConfig {
         applicationId = "com.corridor9design.c9dweather"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         
